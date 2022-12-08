412f5c0efb9afce801ef924dd555ce4f3cc420cf
shobhak6264 <47355074+shobhak6264@users.noreply.github.com>
2022-12-08 14:14:06 -0500

    Inital Commit

---
+++ MMF-Front_End/src/components/charts.js
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from 'react';
+import HighchartsReact from 'highcharts-react-official';
+import Highcharts from 'highcharts';
+import generateData from './generateData';
+import { dummy_data } from '../data/data1';
+import { renderGraphs } from './dynamicdata';
+import { objectData , arrayData , checkGraphType } from './dataHandle';
+
+useEffect = () => {
+  // Render data from API
+}
+export const LineChart = () => {
+  let graphData = [];
+  let categories = [];
+  // renderGraphs(dummy_data,categories,graphData)
+  // console.log("categories",categories,"GraphData",graphData);
+  // const [chartOptions, setChartOptions] = useState({
+  //   title: {text : "Museums Go Forward"},
+  //   xAxis: {
+  //     categories: categories,
+  //   },
+  //   series: [
+  //     { data: graphData }
+  //   ],
+  //   plotOptions: {
+  //     series: {
+  //       point: {
+  //         events: {
+  //           mouseOver(e){
+  //             // setHoverData(e.target.category)
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+    
+  // });
+  return (
+    <React.Fragment>
+    <div class="container">
+      <div className="header-main">
+      <h1 className="header">Museums Moving Forward</h1>
+      <img src=""></img>
+      </div>
+       <div className="graph-display">
+       {dummy_data.map((data, index) => {
+         graphData=[];categories=[];
+         typeof data === 'object' ? objectData(data,graphData,categories) : arrayData(data,graphData,categories,index)
+         console.log("GraphData",graphData , "Categories",categories) 
+         //  generateData(dummy_data,categories,graphData,setChartOptions);
+
+         return (
+            <>
+               <HighchartsReact
+                highcharts={Highcharts}
+                options={{
+                  chart: {
+                    type: checkGraphType(graphData),
+                    panning: true
+                },
+                colors: ['#DB843D', '#AA4643', '#89A54E', '#80699B', '#3D96AE',
+        '#DB843D', '#92A8CD', '#A47D7C', '#B5CA92'],
+                  title: {text : "Question"+"  "+(index+1)},
+                  xAxis: {
+                    categories: categories,
+                  },
+                  series: [
+                    { data: graphData }
+                  ],
+                  plotOptions: {
+                    series: {
+                      point: {
+                        events: {
+                          mouseOver(e){
+                            // setHoverData(e.target.category)
+                          }
+                        }
+                      }
+                    }
+                  },
+                }}
+              />
+            </>
+          )
+        }
+        )
+        }
+        {console.log("GraphData",graphData , "Categories",categories)}
+        {/* <button onClick={getdata} className="graph-button">Update Series</button> */}
+       
+      </div>
+      
+     </div>
+     </React.Fragment>
+     
+      
+    )
+}
---
+++ MMF-Front_End/src/components/dataHandle.js
@@ -0,0 +1,32 @@
+export const objectData = (data,graphData,categories,setgraphType) => {
+    let j=0;
+    let newArray= Object.keys(data);
+        for(j=0;j<newArray.length;j++)
+        {
+        //   console.log(data[newArray[j]])
+           graphData.push(data[newArray[j]]);
+           categories.push(newArray[j]);
+        //    typeof newArray[j] === "string" ? categories.push("Question "+ newArray[j]) :
+        }
+        console.log(graphData.length)
+        
+}
+
+export const arrayData = (data,graphData,categories,index,setgraphType) => {
+    graphData.push(data);
+    categories.push("Question "+ index);
+}
+
+
+export const checkGraphType = (graphData) => {
+    if(graphData.length === 1)
+    {
+        return 'bar';
+    }
+    else if (graphData.length >5){
+        return 'area';
+    }
+    else{
+        return 'spline';
+    }
+}
