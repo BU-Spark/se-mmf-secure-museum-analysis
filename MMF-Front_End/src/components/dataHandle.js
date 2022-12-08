0221652b70fe7a881612cadec6edd74dec5f7706
shobhak6264 <47355074+shobhak6264@users.noreply.github.com>
2022-12-08 15:46:19 -0500

    Updated changes
    1.Modified JSON structure
    2. Restructured the code based on JSON structure
    3. Added new modules for handling data to graphs

--- MMF-Front_End/src/components/dataHandle.js
+++ MMF-Front_End/src/components/dataHandle.js
@@ -1,22 +1,42 @@
-export const objectData = (data,graphData,categories,setgraphType) => {
+export const objectData = (data,graphData,categories) => {
     let j=0;
-    let newArray= Object.keys(data);
+    data.dataSet.forEach((data) => {
+        let newArray= Object.keys(data);
         for(j=0;j<newArray.length;j++)
         {
-        //   console.log(data[newArray[j]])
            graphData.push(data[newArray[j]]);
            categories.push(newArray[j]);
         //    typeof newArray[j] === "string" ? categories.push("Question "+ newArray[j]) :
         }
         console.log(graphData.length)
-        
+    })    
+}
+
+export const getDataset = (data) => {
+    let graphData = [];
+    data.data.forEach((dataset) => {
+        console.log(dataset);
+        graphData.push(dataset);
+    })
+    return graphData;
+}
+
+export const getTitle = (data) => {
+    return data.questionName;
+}
+
+export const getGraphType = (data) => {
+    return data.graphType;
+}
+
+export const getQuestionType = (data) => {
+    return data.questionType;
 }
 
 export const arrayData = (data,graphData,categories,index,setgraphType) => {
     graphData.push(data);
     categories.push("Question "+ index);
 }
-
 
 export const checkGraphType = (graphData) => {
     if(graphData.length === 1)
